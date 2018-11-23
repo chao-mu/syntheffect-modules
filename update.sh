2b9696092e04d5692adc7a9165e100e4f0fb1d1b
Hackpoetic <chao-mu@hackpoetic.com>
2018-11-22 22:56:31 -0500

    Remove binary now no longer referenced

--- update.sh
+++ update.sh
@@ -13,5 +13,5 @@ git remote add upstream https://github.com/chao-mu/syntheffect-workspace
 git pull upstream master
 git submodule update --init --recursive
 
-wget https://github.com/chao-mu/syntheffect/releases/download/v1-pre-alpha.01/syntheffect -O $DIR/bin/syntheffect
-chmod +x $DIR/bin/syntheffect
+#wget https://github.com/chao-mu/syntheffect/releases/download/v1-pre-alpha.01/syntheffect -O $DIR/bin/syntheffect
+#chmod +x $DIR/bin/syntheffect
